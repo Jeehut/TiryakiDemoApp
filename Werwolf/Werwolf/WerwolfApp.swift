736e0ba1449b28174cdeac3fa1811cac95af5415
Cihat Gündüz <jeehut@fline.dev>
2025-09-21 21:04:56 +0200

    Initialize repo

---
+++ Werwolf/Werwolf/WerwolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  WerwolfApp.swift
+//  Werwolf
+//
+//  Created by Cihat Gündüz on 21.09.25.
+//
+
+import SwiftUI
+
+@main
+struct WerwolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
